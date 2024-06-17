fa5e643108eb68d28ab7f79eae0e97916ac4850c
Kilerd Chan <blove694@gmail.com>
2024-06-17 15:36:50 +0800

    feat: add pop_latest method

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,14 @@ use std::hash::Hash;
 #[derive(Clone, Debug)]
 pub struct LatestMap<Key: Eq + Hash + Clone + Ord, Value> {
     pub(crate) data: HashMap<Key, Value>,
-    date_index: BTreeSet<Key>,
+    data_index: BTreeSet<Key>,
 }
 
 impl<Key: Eq + Hash + Clone + Ord, Value> Default for LatestMap<Key, Value> {
     fn default() -> Self {
         Self {
             data: HashMap::new(),
-            date_index: BTreeSet::new(),
+            data_index: BTreeSet::new(),
         }
     }
 }
@@ -19,13 +19,13 @@ impl<Key: Eq + Hash + Clone + Ord, Value> Default for LatestMap<Key, Value> {
 impl<Key: Eq + Hash + Clone + Ord, Value> LatestMap<Key, Value> {
     pub fn insert(&mut self, key: Key, value: Value) {
         self.data.insert(key.clone(), value);
-        self.date_index.insert(key);
+        self.data_index.insert(key);
     }
     pub fn get_latest(&self, key: &Key) -> Option<&Value> {
-        let target_key = if self.date_index.contains(key) {
+        let target_key = if self.data_index.contains(key) {
             key
         } else {
-            let sorted_keys: Vec<&Key> = self.date_index.iter().collect();
+            let sorted_keys: Vec<&Key> = self.data_index.iter().collect();
             match sorted_keys.binary_search(&key) {
                 Ok(_) => key,
                 Err(gt_index) => {
@@ -40,7 +40,7 @@ impl<Key: Eq + Hash + Clone + Ord, Value> LatestMap<Key, Value> {
         self.data.get(target_key)
     }
     pub fn get_last_with_key(&self) -> Option<(&Key, &Value)> {
-        let sorted_keys: Vec<&Key> = self.date_index.iter().collect();
+        let sorted_keys: Vec<&Key> = self.data_index.iter().collect();
         sorted_keys
             .last()
             .and_then(|&key| self.data.get(key).map(|v| (key, v)))
@@ -50,7 +50,26 @@ impl<Key: Eq + Hash + Clone + Ord, Value> LatestMap<Key, Value> {
         self.data.get_mut(key)
     }
     pub fn contains_key(&self, key: &Key) -> bool {
-        self.date_index.contains(key)
+        self.data_index.contains(key)
+    }
+
+    pub fn pop_latest(&mut self, key: &Key) -> Option<Value> {
+        let target_key = if self.data_index.contains(key) {
+            key.clone()
+        } else {
+            let sorted_keys: Vec<&Key> = self.data_index.iter().collect();
+            match sorted_keys.binary_search(&key) {
+                Ok(_) => key.clone(),
+                Err(gt_index) => {
+                    if gt_index == 0 {
+                        return None;
+                    }
+                    sorted_keys[gt_index - 1].clone()
+                }
+            }
+        };
+        self.data_index.remove(&target_key);
+        self.data.remove(&target_key)
     }
 }
 
@@ -62,7 +81,7 @@ mod test {
     fn should_insert() {
         let mut map: LatestMap<i32, i32> = LatestMap::default();
         map.insert(1, 2);
-        assert!(map.date_index.contains(&1));
+        assert!(map.data_index.contains(&1));
         assert_eq!(map.data.get(&1), Some(&2));
     }
 
@@ -106,5 +125,22 @@ mod test {
         assert_eq!(map.get_latest(&0), None);
         assert_eq!(map.get_latest(&1), None);
         assert_eq!(map.get_latest(&3), None);
+    }
+
+    #[test]
+    fn should_work_pop_latest() {
+        let mut map: LatestMap<i32, i32> = LatestMap::default();
+        map.insert(1, 2);
+        map.insert(10, 20);
+        map.insert(20, 40);
+        map.insert(50, 100);
+
+        assert_eq!(map.get_latest(&0), None);
+        assert_eq!(map.data.len(), 4);
+        assert_eq!(map.get_latest(&1), Some(&2));
+        assert_eq!(map.data.len(), 4);
+        assert_eq!(map.pop_latest(&3), Some(2));
+        assert_eq!(map.data.len(), 3);
+        assert_eq!(map.data_index.len(), 3);
     }
 }
